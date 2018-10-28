afc772002c34f172dd61750e2597df954749763c
Miguel Montalvo <pomodroizer@gmail.com>
2018-10-28 22:01:08 +0000

    Working version of noughts & crosses in Go

--- cli.go
+++ cli.go
@@ -5,32 +5,61 @@ import (
     "strconv"
     "strings"
     "os"
-    "math/rand"
 )
+
+// Missing:
+// - do input validation
+// - split into other packages (game and board)
+// - improve checkWinner
 
 var winner bool = false
 var current_player string = "x"
 var other_player   string = "o"
+var board = [9]string{" "," "," "," "," "," "," "," "," "}
 
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
-    board := [3][3]string{}
     var text string
     for winner != true {
-        printBoard(board)
         fmt.Print("Enter coordinates (row,col): ")
         scanner.Scan()
         text = scanner.Text()
         row, column := getScanCoordinates(text)
-        board[row][column] = current_player
-        if checkWinner() {
-            winner = true
+        if (board[(row * 3) + column]) != " "{
+          fmt.Println("Coordinates are not empty, please choose a new one:")
+        }else{
+          board[(row * 3) + column] = current_player
+          if checkWinner() {
+              winner = true
+              fmt.Println("WINNER!", current_player)
+          }
+          current_player, other_player = other_player, current_player
         }
+        printBoard(board)
     }
 }
 
+func winnerCombination(combination [3]int) bool {
+  if (board[combination[0]] == current_player &&
+      board[combination[1]] == current_player &&
+      board[combination[2]] == current_player){
+    return true
+  }
+  return false
+}
+
 func checkWinner() bool{
-  return (rand.Intn(100) % 2) == 0
+  if (winnerCombination([3]int {0, 1, 2}) ||
+      winnerCombination([3]int {3, 4, 5}) ||
+      winnerCombination([3]int {6, 7, 8}) ||
+      winnerCombination([3]int {0, 3, 6}) ||
+      winnerCombination([3]int {1, 4, 7}) ||
+      winnerCombination([3]int {2, 5, 8}) ||
+      winnerCombination([3]int {0, 4, 8}) ||
+      winnerCombination([3]int {2, 4, 6})) {
+       return true
+     }
+  return false
 }
 
 func getScanCoordinates(input string) (int, int){
@@ -40,6 +69,8 @@ func getScanCoordinates(input string) (int, int){
   return row, column
 }
 
-func printBoard(board [3][3]string) {
-  fmt.Println(board)
+func printBoard(board [9]string) {
+  for i := 0; i < 3; i++ {
+		fmt.Printf("%s %s %s\n", board[(i * 3) + 0], board[(i * 3) + 1], board[(i * 3) + 2])
+	}
 }
